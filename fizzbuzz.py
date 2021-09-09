4f1f41c71cdf1cb16d9b143c8dd343d5f4963ee0
bickhaus <bickhaus@users.noreply.github.com>
2021-09-08 22:05:10 -0500

    removed commented code

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,16 +1,5 @@
 #fizzbuzz
 
-#for i in range(1,101):
-#    if i % 3 == 0:
-#        if i % 5 == 0:
-#            print("FizzBuzz")
-#        else:
-#            print("Fizz")
-#    elif i % 5 == 0:
-#        print("Buzz")
-#    else:
-#        print(i)
-
 for i in range(1,101):
     fbString = ""
     if i % 3 == 0:
